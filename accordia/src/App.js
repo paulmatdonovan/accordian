a6087408e685e777397e11d98bcf65ef916f6fbc
Paul Donovan <paul.m.donovan@outlook.com>
2024-09-05 21:41:56 +1000

    Started project

---
+++ accordia/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Accordian from './components/Accordina';
+
+function App() {
+  return (
+    <Accordian />
+  );
+}
+
+export default App;
---
+++ accordia/src/components/Accordina/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used for organisation"
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "Accordian components are user interface elements used for organisation"
+    },
+    {
+        id: '3',
+        question: "Accordians are muscial instruments?",
+        answer: "Accordian components are user interface elements used for organisation"
+    },
+    {
+        id: '4',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used for organisation"
+    },
+]
+export default data;
